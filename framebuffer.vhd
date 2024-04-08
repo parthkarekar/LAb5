b0d10b0d959a1626d0d5a0c3ea5d0b1a8ed37002
parthkarekar <158207198+parthkarekar@users.noreply.github.com>
2024-04-08 18:50:44 -0400

    Add files via upload

--- framebuffer.vhd
+++ framebuffer.vhd
@@ -2,7 +2,7 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 04/04/2024 08:46:49 PM
+-- Create Date: 04/08/2024 05:39:05 PM
 -- Design Name: 
 -- Module Name: framebuffer - Behavioral
 -- Project Name: 
@@ -28,31 +28,58 @@ use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
-library UNISIM;
-use UNISIM.VComponents.all;
+--library UNISIM;
+--use UNISIM.VComponents.all;
 
 entity framebuffer is
-    
-    generic (
-    
-        DATA : integer := 72;
-        ADDR : integer := 10
-    
-    );
-
-
-    Port ( clk1, en1, en2 : in STD_LOGIC;
-           addr1, addr2 : out STD_LOGIC_VECTOR (11 downto 0);
+    Port ( clk1, en1, en2, ld : in STD_LOGIC;
+           addr1, addr2 : in STD_LOGIC_VECTOR (11 downto 0);
            wr_en1 : in STD_LOGIC;
-           din1 : in STD_LOGIC_VECTOR (15 downto 0);
+           din : in STD_LOGIC_VECTOR (15 downto 0);
            dout1, dout2 : out STD_LOGIC_VECTOR (15 downto 0));
 end framebuffer;
 
 architecture Behavioral of framebuffer is
 
+type mem_array is array (0 to 4095) of std_logic_vector (15 downto 0);
+signal memory: mem_array;
+
 begin
 
+process(clk1)
+begin  
 
-
+  if (rising_edge (clk1) ) then 
+    
+    if( ld = '1') then 
+    
+        if ( en1 = '1') then 
+        
+            if (wr_en1 = '1') then 
+            
+                memory(to_integer(unsigned(addr1))) <= din;
+         
+            end if;
+            
+            dout1 <= memory(to_integer(unsigned(addr1)));
+            
+        elsif (en2 = '1') then 
+        
+            if(wr_en1 = '1') then 
+            
+                memory(to_integer(unsigned(addr2))) <= din;
+                
+            end if; 
+            
+            dout2 <= memory(to_integer(unsigned(addr2)));
+            
+        end if;
+        
+    end if; 
+      
+  end if;
+    
+ end process; 
+ 
 
 end Behavioral;
